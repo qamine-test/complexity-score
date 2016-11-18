cff41e16490823b36fa8662fd4415324aac18e1e
Rodrigo Fernandes <rodrigo.fernandes@tecnico.ulisboa.pt>
2016-11-18 15:33:59 +0000

    Useless change

--- sieve-of-eratosthenes/sieve.py
+++ sieve-of-eratosthenes/sieve.py
@@ -1,6 +1,6 @@
 def eratosthenes2(n):
     multiples = set()
-    for i in range(2, n+1):
+    for i in range(2, n+2):
         if i not in multiples:
             yield i
             multiples.update(range(i*i, n+1, i))
