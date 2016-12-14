21a3860e8a3e13441a7f775f296184dfc6f83c21
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2016-12-14 13:42:20 +0000

    Add ignores

---
+++ new/sieve-of-eratosthenes/sieve.py
@@ -0,0 +1,19 @@
+def eratosthenes2(n):
+    multiples = set()
+    for i in range(2, n+1):
+        if i not in multiples:
+            yield i
+            multiples.update(range(i*i, n+1, i))
+    for i in range(2, n+1):
+        if i not in multiples:
+            yield i
+            multiples.update(range(i*i, n+1, i))
+
+def eratosthenesFastNotReally(n):
+    multiples = set()
+    for i in range(2, n+1):
+        if i not in multiples:
+            yield i
+            multiples.update(range(i*i, n+1, i)) 
+ 
+print(list(eratosthenes2(100)))
